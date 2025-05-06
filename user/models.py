9ffb6d670ac1b1edf44d63d491d3c8c0b64ef57b
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-05-06 22:16:40 +0300

    9

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from PIL import Image
 # Create your models here.
 
 
@@ -11,6 +11,14 @@ class Profile(models.Model):
  image=models.ImageField(default="default.jpg",upload_to="profile_pics")
  def __str__(self):
   return f'{ self.user_id.username} profile '
+  
+ def save(self):
+        super().save()
+        img=Image.open(self.image.path)
+        if img.height>300 or img.width>300:
+            size=(300,300)
+            img.thumbnail(size)
+            img.save(self.image.path)
 
 User._meta.get_field('email')._unique = True
 # python manage.py migrate --fake 
