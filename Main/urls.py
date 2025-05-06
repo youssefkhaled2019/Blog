6c0c5f550a405ca8663b7ca4ec1f7af0d06274fc
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-05-06 20:47:55 +0300

    8

--- Main/urls.py
+++ Main/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from user import views as users_views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),#blog/
@@ -28,3 +29,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html",next_page='login') ,name="logout"),
 
 ]
+if settings.DEBUG:
+   urlpatterns+=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/models.py
+++ user/models.py
@@ -5,5 +5,12 @@ from django.db import models
 
 # unique email
 from django.contrib.auth.models import User
-User._meta.get_field('email')._unique = True
 
+class Profile(models.Model):
+ user_id=models.OneToOneField(User,on_delete=models.CASCADE)
+ image=models.ImageField(default="default.jpg",upload_to="profile_pics")
+ def __str__(self):
+  return f'{ self.user_id.username} profile '
+
+User._meta.get_field('email')._unique = True
+# python manage.py migrate --fake 
